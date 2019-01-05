dc161f57b425fd9507985864492bd4eaa2e2962b
young <yangxu@yeezon.com>
2019-01-06 00:31:37 +0800

    feat: TransitionGroup

--- animation/src/App.js
+++ animation/src/App.js
@@ -1,36 +1,43 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import { CSSTransition } from 'react-transition-group';
+import { TransitionGroup, CSSTransition} from 'react-transition-group';
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      show: true
-    }
-    this.handleToggle = this.handleToggle.bind(this);
+      list: ['item']
+    };
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
 
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.show}
-          timeout={1000}
-          classNames="fade"
-          appear={true}
+        <TransitionGroup
           >
-          <div>
-            hello
-          </div>
-        </CSSTransition>
-        <button onClick={this.handleToggle}>toggle</button>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  key={index}
+                  timeout={1000}
+                  classNames='fade'>
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     );
   }
 
-  handleToggle () {
-    this.setState({
-      show: !this.state.show
+  handleAddItem () {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      }
     });
   }
 }
